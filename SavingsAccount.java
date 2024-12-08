ef82085f94c1808f15afd0ff76ebe35d003cc677
prudvi <prudvir529@gmail.com>
2024-12-08 07:16:51 +0530

    added savingsAccount file

---
+++ SavingsAccount.java
@@ -0,0 +1,33 @@
+package com.banking;
+
+public class SavingsAccount implements Banking_Operations {
+    private double balance;
+    
+    public SavingsAccount() {
+        this.balance = 0.0;
+    }
+    
+    
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+            System.out.println("Deposited: " + amount);
+        } else {
+            System.out.println("Invalid deposit amount.");
+        }
+    }
+    
+    
+    public void withdraw(double amount) {
+        if (amount > 0 && amount <= balance) {
+            balance -= amount;
+            System.out.println("Withdrawn: " + amount);
+        } else {
+            System.out.println("Insufficient balance or invalid amount.");
+        }
+    }
+    
+    public double viewBalance() {
+        return balance;
+    }
+}
